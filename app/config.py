016c00ae18d6771fbe277c7c18ecef720e44733e
lixuesong <lixuesong@bytedance.com>
2017-04-18 11:15:20 +0800

    mircoblog web表单

---
+++ app/config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
+    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
+    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
+    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
+    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+#CSRF_ENABLED 配置是为了激活 跨站点请求伪造 保护
+#SECRET_KEY 配置仅仅当 CSRF 激活的时候才需要，它是用来建立一个加密的令牌，用于验证一个表单
