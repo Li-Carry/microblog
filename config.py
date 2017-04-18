ef3a601b553dcc1366839b95176dc1f56318a91e
lixuesong <lixuesong@bytedance.com>
2017-04-18 17:00:24 +0800

    登数据库录

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
@@ -9,6 +10,15 @@ OPENID_PROVIDERS = [
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+    
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-#CSRF_ENABLED 配置是为了激活 跨站点请求伪造 保护
-#SECRET_KEY 配置仅仅当 CSRF 激活的时候才需要，它是用来建立一个加密的令牌，用于验证一个表单
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['lixuesong@bytedance.com']
